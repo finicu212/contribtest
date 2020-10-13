d479b8ee439008a3bc0b23adf86cad6d465ba883
finicu212 <smurfdeplastic@gmail.com>
2020-10-13 17:52:54 +0300

    test_generate_site is now more general made it possible to add more files in expected_output which will be used in the tests

--- test_generate.py
+++ test_generate.py
@@ -1,12 +1,30 @@
 import unittest
 import generate
 import filecmp
+import os
+from unittest import mock
 
 class TestGenerate(unittest.TestCase):
 	def test_generate_site_from_example(self):
-		result = generate.generate_site("test/source", "output")
-		self.assertTrue(filecmp.cmp("output/index.html", "test/expected_output/index.html"), "Test fail: Unexpected output in index.html")
-		self.assertTrue(filecmp.cmp("output/contact.html", "test/expected_output/contact.html"), "Test fail: Unexpected output in contact.html")
+		source_dir = "test/source"
+		expected_dir = "test/expected_output"
+		output_dir = "output"
+		result = generate.generate_site(source_dir, output_dir)
+
+		# for each file which we expect
+		for f in os.listdir(expected_dir):
+			outputted_file = os.path.join(output_dir, f)
+			# make sure that the file actually got created
+			self.assertTrue(os.path.isfile(outputted_file), "File " + f + " did not get created!")
+			# make sure that the file contents are identical
+			self.assertTrue(filecmp.cmp(outputted_file, os.path.join(expected_dir, f)), "Test fail: Unexpected output in " + f)
+
+	#@mock.patch('generate.read_file')
+	#def test_reading(self, read_file_mock):
+	#	with open("example.rst", "w") as f:
+	#		f.write('{"title": "example", "layout": "example.html"} --- content example here')
+
+
 
 # to be able to just run `python test_generate.py` directly
 if __name__ == '__main__':
